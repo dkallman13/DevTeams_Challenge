d23a2972c232ad0ffae17a5b96078d4cf510eb1c
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2021-08-13 15:36:33 -0400

    Add project files.

---
+++ DevTeams_Challenge_Console/ProgramUI.cs
@@ -0,0 +1,45 @@
+﻿using DevTeams_Challenge_Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeams_Challenge_Console
+{
+    public class ProgramUI
+    {
+        //This class will be how we interact with our user through the console. When we need to access our data, we will call methods from our Repo class.
+
+        private DevTeamsRepo _repo = new DevTeamsRepo();
+
+        public void Run()
+        {
+            //SeedContent();
+            Menu();
+        }
+
+        private void Menu()
+        {
+            //Start with the main menu here
+            //Menu options we'll need and the methods you'll need to make
+
+            //CreateNewDeveloper();
+            //DisplayAllDevelopers();
+            //DisplayDevelopersByTeam();
+            //DisplayDeveloperById();
+            //UpdateExistingDeveloper();
+            //DeleteExistingDeveloper();
+        }
+
+        // Helpermethods you should write
+        // A method to print a developer's first and last name, useful in display all
+        // private void DisplayDevBasicInfo(Developer dev) {}
+
+        // A method to print a developers full information, useful when showing one developer
+        // private void DisplayDevFullInfo(Developer dev) {}
+
+
+        //private void SeedContent(){}
+    }
+}
---
+++ DevTeams_Challenge_Repository/DevTeamsRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeams_Challenge_Repository
+{
+    public class DevTeamsRepo
+    {
+        //This is our Repository class that will hold our directory (which will act as our database) and methods that will directly talk to our directory.
+
+        private List<Developer> _devDirectory = new List<Developer>();
+
+        // C
+        
+        // R
+        
+        // U
+
+        // D
+    }
+}
