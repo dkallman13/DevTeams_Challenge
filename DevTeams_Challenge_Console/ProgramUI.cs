2c33a941e7030ceeaf6cfef54f8916e4c8b86c0a
Dan Kallman <dkallman13@gmail.com>
2021-08-16 17:08:52 -0400

    1/2 way done

--- DevTeams_Challenge_Console/ProgramUI.cs
+++ DevTeams_Challenge_Console/ProgramUI.cs
@@ -15,12 +15,52 @@ namespace DevTeams_Challenge_Console
 
         public void Run()
         {
-            //SeedContent();
+            SeedContent();
             Menu();
         }
 
         private void Menu()
         {
+            bool continueRunning = true;
+            while (continueRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu (select one):\n" +
+                    "1. Add Developer\n" +
+                    "2. Display All Developers \n" +
+                    "3. Display Developers By Teams \n" +
+                    "4. Display Developers By ID \n" +
+                    "5. Update developer \n" +
+                    "6. Delete Existing Developer\n" +
+                    "7. Exit");
+
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        CreateNewDeveloper();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        GetDeveloperByTeam();
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        continueRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid number(1-7)");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
             //Start with the main menu here
             //Menu options we'll need and the methods you'll need to make
 
@@ -31,6 +71,138 @@ namespace DevTeams_Challenge_Console
             //UpdateExistingDeveloper();
             //DeleteExistingDeveloper();
         }
+        private void CreateNewDeveloper()
+        {
+            Console.Clear();
+            bool isRunning = true;
+            string firstname;
+            int idInt;
+            string lastname;
+            TeamAssignment team;
+            bool canAccessPluralsight;
+            while (isRunning)
+            {
+                Console.WriteLine("What is their ID?");
+                string id = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(id) || string.IsNullOrWhiteSpace(id))
+                {
+                    Console.WriteLine("Please input a valid number");
+                    break;
+                }
+                else
+                {
+                    idInt = int.Parse(id);
+                    Console.WriteLine("What is their first name?");
+                    firstname = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(firstname) || string.IsNullOrEmpty(firstname))
+                    {
+                        Console.WriteLine("Please input a name.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("What is their last name?");
+                        lastname = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(lastname) || string.IsNullOrEmpty(lastname))
+                        {
+                            Console.WriteLine("Please input a name.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Which team are they on?\n" +
+                                "1. Front-End\n" +
+                                "2. Back-End\n" +
+                                "3. Testing");
+                            int response = int.Parse(Console.ReadLine());
+                            if (response <= 3 && response >= 1)
+                            {
+                                team = (TeamAssignment)response;
+                                Console.WriteLine("Can they access Pluralsight? enter y for yes or n for no");
+                                string access = Console.ReadLine().ToLower();
+                                if (access == "y")
+                                {
+                                    canAccessPluralsight = true;
+                                    isRunning = false; 
+
+                                }
+                                else if (access == "n")
+                                {
+                                    canAccessPluralsight = false;
+                                    isRunning = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("please enter y or n");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("please input a number from 1-3");
+                                break;
+                            }
+
+
+                        }
+                    }
+
+                }
+                Developer dev = new Developer(idInt, firstname, lastname, canAccessPluralsight, team);
+
+                if (_repo.AddContentToDirectory(dev))
+                {
+                    Console.WriteLine("successfully added to the dev list");
+                }
+                else
+                {
+                    Console.WriteLine("mr programmer screwed up");
+                }
+                Console.ReadKey();
+            }
+        }
+        private void GetDeveloperByTeam()
+        {
+            Console.Clear();
+            Console.WriteLine("What team do you want to search for?\n" +
+                "1. Front-End\n" +
+                "2. Back-End\n" +
+                "3. Testing\n");
+            string serachTerms = Console.ReadLine();
+            TeamAssignment teamSearch = 0;
+            switch (serachTerms)
+            {
+                case "1":
+                    teamSearch = TeamAssignment.FrontEnd;
+                    break;
+                case "2":
+                    teamSearch = TeamAssignment.BackEnd;
+                    break;
+                case "3":
+                    teamSearch = TeamAssignment.Testing;
+                    break;
+                default:
+                    Console.WriteLine("Please input a number from 1 - 3");
+                    break;
+            }
+            List<Developer> results = _repo.GetDevelopersByTeam(teamSearch);
+            if (results != null)
+            {
+                Console.WriteLine($"{"ID",-5} |{"First Name",-10} |{"Last Name",-10} |{"Team Assignment",-10} |{"Pluralsight access",-5}");
+                Console.WriteLine("..............................................................................................");
+                foreach (Developer result in results)
+                {
+                    Console.WriteLine($"{result.ID,-5} {result.FirstName,-10} {result.LastName,-10} {result.Team,-20} {result.CanAccessPluralsight,-25}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("no results for that team");
+            }
+            Console.ReadKey();
+        }
 
         // Helpermethods you should write
         // A method to print a developer's first and last name, useful in display all
@@ -40,6 +212,14 @@ namespace DevTeams_Challenge_Console
         // private void DisplayDevFullInfo(Developer dev) {}
 
 
-        //private void SeedContent(){}
+        private void SeedContent()
+        {
+            Developer developerOne = new Developer(1, "Bob", "Dave", true, TeamAssignment.BackEnd);
+            Developer developerTwo = new Developer(2, "Chad", "Johnson", true, TeamAssignment.FrontEnd);
+            Developer developerThree = new Developer(3, "Ricky", "Bobby", false, TeamAssignment.Testing);
+            _repo.AddContentToDirectory(developerOne);
+            _repo.AddContentToDirectory(developerTwo);
+            _repo.AddContentToDirectory(developerThree);
+        }
     }
 }
--- DevTeams_Challenge_Repository/Developer.cs
+++ DevTeams_Challenge_Repository/Developer.cs
@@ -6,16 +6,32 @@ using System.Threading.Tasks;
 
 namespace DevTeams_Challenge_Repository
 {
-    public enum TeamAssignment { FrontEnd, BackEnd, Testing}
+    public enum TeamAssignment { FrontEnd = 1, BackEnd, Testing }
     public class Developer
     {
+
         //This is our POCO class. It will define our properties and constructors of our Developer objects.
         //Developer objects should have the following properties
-            //ID (int)
-            //FirstName
-            //LastName
-            //a bool that shows whether they have access to the online learning tool Pluralsight.
-            //TeamAssignment - use the enum declared above this class
+        //ID (int)
+        //FirstName
+        //LastName
+        //a bool that shows whether they have access to the online learning tool Pluralsight.
+        //TeamAssignment - use the enum declared above this class
+        public int ID { get; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool CanAccessPluralsight { get; set; }
+        public TeamAssignment Team { get; set; }
+
+        public Developer(int id, string first, string last, bool hasPluralsightAccess, TeamAssignment team)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            CanAccessPluralsight = hasPluralsightAccess;
+            Team = team;
+        }
+
+
     }
 }
